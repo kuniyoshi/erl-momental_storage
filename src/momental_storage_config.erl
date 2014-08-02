679d20ed39e72216a74992e4c97197e57e582b03
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2014-08-02 23:11:02 +0900

    Specify chunk config from config module.

--- src/momental_storage_config.erl
+++ src/momental_storage_config.erl
@@ -1,6 +1,9 @@
 -module(momental_storage_config).
 -export([hostname/0,
-         port/0]).
+         port/0,
+         max_transfer_size/0,
+         fixed_chunk_size/0,
+         timeout_ms/0]).
 
 from_application_env(Key) ->
     {ok, Val} = application:get_env(momental_storage, Key),
@@ -11,3 +14,12 @@ hostname() ->
 
 port() ->
     from_application_env(port).
+
+max_transfer_size() ->
+    500000000.
+
+fixed_chunk_size() ->
+    100000.
+
+timeout_ms() ->
+    180000.
