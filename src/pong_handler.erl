9a31a124daabd9b42c742b4db7a642e0a49fc57b
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2014-08-03 18:29:25 +0900

    Use SSL default, and add a testable URL.

---
+++ src/pong_handler.erl
@@ -0,0 +1,17 @@
+-module(pong_handler).
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+
+init(_Transport, Req, []) ->
+    {ok, Req, undefined}.
+
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200,
+                                  [{<<"content-type">>, <<"text/plain">>}],
+                                  <<"pong.\n">>,
+                                  Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
